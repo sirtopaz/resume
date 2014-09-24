31c6a5e38c7711870ff0a0224859b6deb563d704
Sean Gervais <github@virtualsean.com>
2014-09-24 09:33:53 -0400

    Updated dependencies and job desc

--- app/models/job.js
+++ app/models/job.js
@@ -19,7 +19,7 @@ Job.reopenClass({
 		{id:6, company:"ScanScout Inc.", title:"Front-end Development Manager", startDate:"2008-07-01", endDate:"2009-06-30", technologies:"HTML, CSS, JavaScript, JSON, JQuery, JQuery UI, Java, JSP, JSTL, Spring Framework (MVC), Hibernate, Fusion Charts, Flash (ActionScript), Photoshop, Ecplise, SVN"},
 		{id:7, company:"Redi2 Technologies", title:"Senior Web/UI Engineer", startDate:"2009-07-01", endDate:"2011-05-31", technologies:"HTML, CSS, JavaScript, JSON, JQuery, JQuery UI, Java, JSP, JSTL, Spring Framework (MVC), Hibernate, Dreamweaver, Photoshop, Illustrator, Eclipse, Balsamiq Mockups, Visio, SVN"},
 		{id:8, company:"Core Security", title:"Development Manager (UI)", startDate:"2011-05-01", endDate:"2013-07-31", technologies:"HTML5, CSS3, JavaScript, SVG, Adobe Flex, Java, AJAX, JSON, JQuery, D3js, Spring Framework (MVC), Hibernate, Eclipse, Dreamweaver, Photoshop, Illustrator, InDesign, Balsamiq Mockups, Visio, SVN"},
-		{id:9, company:"McGraw-Hill Education", title:"Director of Engineering, Analytics", startDate:"2013-08-01", endDate:null, technologies:"HTML5, CSS3, SVG, JavaScript, JSON, SASS, JQuery, EmberJS, D3js, Foundation, Sublime Text, Photoshop, Illustrator, Dreamweaver, InDesign, Github"}
+		{id:9, company:"McGraw-Hill Education", title:"Director of Engineering, Analytics", startDate:"2013-08-01", endDate:null, technologies:"HTML5, CSS3, SVG, JavaScript, JSON, SASS, JQuery, EmberJS, D3js, Foundation, Bower, NodeJS, Sublime Text, Photoshop, Illustrator, Dreamweaver, InDesign, Github"}
 	]
 });
 
